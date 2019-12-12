d0051f36f05348c5e2cae3409436558ee4d79911
dabakura <davidcamposch@gmail.com>
2019-12-11 22:15:31 -0600

    integracion con monodevelop

--- Curriculum/Curriculum/Startup.cs
+++ Curriculum/Curriculum/Startup.cs
@@ -63,7 +63,8 @@ namespace Curriculum
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+                    //spa.UseAngularCliServer(npmScript: "start");
                 }
             });
         }
