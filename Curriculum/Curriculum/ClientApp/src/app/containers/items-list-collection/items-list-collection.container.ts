c720b50573e9126a876b82e9f23b9318d06ab166
dabakura <davidcamposch@gmail.com>
2019-12-21 00:07:47 -0600

    footer

---
+++ Curriculum/Curriculum/ClientApp/src/app/containers/items-list-collection/items-list-collection.container.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-items-list-collection',
+  templateUrl: './items-list-collection.component.html',
+  styleUrls: ['./items-list-collection.component.css']
+})
+export class ItemsListCollectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
