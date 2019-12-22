ab6700544fe8a934ddd4437360c8dbb0b74b9003
dabakura <davidcamposch@gmail.com>
2019-12-21 20:02:28 -0600

    download pdf

--- Curriculum/Curriculum/ClientApp/src/app/components/nav-menu/nav-menu.component.ts
+++ Curriculum/Curriculum/ClientApp/src/app/components/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Component({
   selector: "app-nav-menu",
@@ -6,8 +7,32 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./nav-menu.component.css"]
 })
 export class NavMenuComponent implements OnInit {
+  @ViewChild("downloadZipLink", { static: true })
+  private downloadLink: ElementRef;
+  constructor(private http: HttpClient) {}
   ngOnInit(): void {
-    var elems = document.querySelectorAll(".sidenav");
-    var instances = M.Sidenav.init(elems);
+    const elems = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(elems);
+  }
+  public async downloadResource(): Promise<Blob> {
+    let headers = new HttpHeaders();
+    headers = headers.set("Accept", "application/pdf");
+    const url = "/assets/pdf/curriculum-vitae-2019.pdf";
+    const file = await this.http
+      .get<Blob>(url, { headers: headers, responseType: "blob" as "json" })
+      .toPromise();
+    return file;
+  }
+  /**
+   * DowmloadPDF
+   */
+  public async DowmloadPDF() {
+    const blob = await this.downloadResource();
+    const url = window.URL.createObjectURL(blob);
+    const link = this.downloadLink.nativeElement;
+    link.href = url;
+    link.download = "Curriculum.pdf";
+    link.click();
+    window.URL.revokeObjectURL(url);
   }
 }
