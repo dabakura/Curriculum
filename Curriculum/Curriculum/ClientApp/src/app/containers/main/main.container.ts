b303f229b2625dbbe02889f93d4ab0b02bae6705
dabakura <davidcamposch@gmail.com>
2019-12-30 17:50:16 -0600

    main fix for movil

--- Curriculum/Curriculum/ClientApp/src/app/containers/main/main.container.ts
+++ Curriculum/Curriculum/ClientApp/src/app/containers/main/main.container.ts
@@ -1,8 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Observable, fromEvent } from "rxjs";
+import { startWith, map } from "rxjs/operators";
 
 @Component({
   selector: "app-main",
   templateUrl: "./main.container.html",
   styleUrls: ["./main.container.css"]
 })
-export class MainContainer {}
+export class MainContainer implements OnInit {
+  isMobile = false;
+  getIsMobile(): boolean {
+    const w = document.documentElement.clientWidth;
+    const breakpoint = 960;
+    return w < breakpoint;
+  }
+
+  ngOnInit() {
+    this.isMobile = this.getIsMobile();
+    window.onresize = () => {
+      this.isMobile = this.getIsMobile();
+    };
+  }
+}
