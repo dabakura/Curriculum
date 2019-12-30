0bf7c737171783a614efb4aa3b98d99fa8657477
dabakura <davidcamposch@gmail.com>
2019-12-30 03:10:52 -0600

    Initials job personal component

---
+++ Curriculum/Curriculum/ClientApp/src/app/components/personal-item/personal-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-item',
+  templateUrl: './personal-item.component.html',
+  styleUrls: ['./personal-item.component.css']
+})
+export class PersonalItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
