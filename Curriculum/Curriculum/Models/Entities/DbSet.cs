2b01363356d54007a4e21afebbadd4206304f51f
dabakura <davidcamposch@gmail.com>
2020-01-24 00:06:46 -0600

    change icon format

--- Curriculum/Curriculum/Models/Entities/DbSet.cs
+++ Curriculum/Curriculum/Models/Entities/DbSet.cs
@@ -26,7 +26,7 @@ namespace Curriculum.Models.Entities
          {
             if ("Knowledge" == typeof(T).Name)
             {
-               return "{ \"Frameworks\" : [ { \"Imagen\" : \"angular.png\", \"Level\" : 7, \"Name\" : \"Angular\" } ],\"Languages\" : [ {\"Imagen\" : \"espanol.png\",\"Level\" : 10,\"Name\" : \"Español\"}, {\"Imagen\" : \"ingles.png\", \"Level\" : 6,\"Name\" : \"Ingles\"} ],\"Otros\" : [ {\"Description\" : \"XML, JSON\",\"Name\" : \"Formatos\"} ],\"Programming_Languages\" : [ {\"Imagen\" : \"Csharp.png\",\"Level\" : 8.5,\"Name\" : \"C#\"} ],\"Technologies\" : [ {\"List\" : [ \"JavaFX\", \"JavaEE\", \"Java Web\", \"ASP\", \"Android\", \"WinForm\", \"WPF\", \".Net-Core\" ],\"Name\" : \"Programación\"}]}";
+               return "{ \"Frameworks\" : [ { \"Imagen\" : \"angular.webp\", \"Level\" : 7, \"Name\" : \"Angular\" } ],\"Languages\" : [ {\"Imagen\" : \"espanol.webp\",\"Level\" : 10,\"Name\" : \"Español\"}, {\"Imagen\" : \"ingles.webp\", \"Level\" : 6,\"Name\" : \"Ingles\"} ],\"Otros\" : [ {\"Description\" : \"XML, JSON\",\"Name\" : \"Formatos\"} ],\"Programming_Languages\" : [ {\"Imagen\" : \"Csharp.webp\",\"Level\" : 8.5,\"Name\" : \"C#\"} ],\"Technologies\" : [ {\"List\" : [ \"JavaFX\", \"JavaEE\", \"Java Web\", \"ASP\", \"Android\", \"WinForm\", \"WPF\", \".Net-Core\" ],\"Name\" : \"Programación\"}]}";
             }
             if ("Information" == typeof(T).Name)
             {
@@ -34,15 +34,15 @@ namespace Curriculum.Models.Entities
             }
             if ("Reference" == typeof(T).Name)
             {
-               return "{\"Jobs\":[{\"Admission\": \"12/04/2017\",\"Departure\": \"22/06/2019\",\"Company\": \"C&D Soporte Técnico\",\"Position\": \"Developer and Technical Support\",\"Description\": \"Tareas realizadas: Mantenimiento preventivo, Mantenimiento correctivo, Configuración de redes, Reparación de dispositivos electrónicos, programación, asesoramiento, entre otros.\",\"Phone\": \"23457845\" } ],\"Personals\":[{\"Name\": \"Elva Lopez\",\"Phone\": \"23457845\",\"Image\": \"cartadeelva.png\",\"Pdf\": \"cartadeelva.pdf\"} ]}";
+               return "{\"Jobs\":[{\"Admission\": \"12/04/2017\",\"Departure\": \"22/06/2019\",\"Company\": \"C&D Soporte Técnico\",\"Position\": \"Developer and Technical Support\",\"Description\": \"Tareas realizadas: Mantenimiento preventivo, Mantenimiento correctivo, Configuración de redes, Reparación de dispositivos electrónicos, programación, asesoramiento, entre otros.\",\"Phone\": \"23457845\" } ],\"Personals\":[{\"Name\": \"Elva Lopez\",\"Phone\": \"23457845\",\"Image\": \"cartadeelva.webp\",\"Pdf\": \"cartadeelva.pdf\"} ]}";
             }
             if ("Project" == typeof(T).Name)
             {
-               return "[ {\"Built\" : {\"Github\" : \"https://localhost/githup/medical\",\"Link\" : \"https://localhost:5001\",\"ListBuilt\" : [ {\"Image\" : \"angular.png\",\"Name\" : \"Angular\"}, {\"Image\" : \"c++.png\",\"Name\" : \"C++\"}, {\"Image\" : \"js.png\",\"Name\" : \"Javascript\" } ],\"Type\" : \"Personal\"},\"Creation_Date\" : \"2012-04-23\",\"Description\" : \"Biblioteca de formulas, para el uso farmaceutico en pacientes de pediatria\",\"OnlyImage\" : false,\"Title\" : \"Medical\", \"Video\" : \"big_buck_bunny.mp4\"}]";
+               return "[ {\"Built\" : {\"Github\" : \"https://localhost/githup/medical\",\"Link\" : \"https://localhost:5001\",\"ListBuilt\" : [ {\"Image\" : \"angular.webp\",\"Name\" : \"Angular\"}, {\"Image\" : \"c++.webp\",\"Name\" : \"C++\"}, {\"Image\" : \"js.webp\",\"Name\" : \"Javascript\" } ],\"Type\" : \"Personal\"},\"Creation_Date\" : \"2012-04-23\",\"Description\" : \"Biblioteca de formulas, para el uso farmaceutico en pacientes de pediatria\",\"OnlyImage\" : false,\"Title\" : \"Medical\", \"Video\" : \"big_buck_bunny.mp4\"}]";
             }
             if ("Certification" == typeof(T).Name)
             {
-               return "[{\"Date\": \"08/12/2018\",\"Institution\": \"Universidad Fidelitas\",\"Title\": \"CCNA 1\",\"Image\": \"ccna1.png\",\"Description\": \"Enrutamiento y creacion de redes bla bla bla\",\"Ppf\": \"ccna1.pdf\"}]";
+               return "[{\"Date\": \"08/12/2018\",\"Institution\": \"Universidad Fidelitas\",\"Title\": \"CCNA 1\",\"Image\": \"ccna1.webp\",\"Description\": \"Enrutamiento y creacion de redes bla bla bla\",\"Ppf\": \"ccna1.pdf\"}]";
             }
             return "[]";
             //throw;
